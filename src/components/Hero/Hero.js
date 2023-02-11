70c8222d2df8a9b4187bbaf797a5958d15780077
Yash <yashvaghela015@gmail.com>
2023-02-11 09:11:07 +0530

    first commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = (props) => (
       My Personal Portfolio
     </SectionTitle>
     <SectionText>
-      I do web development & machine learning
+    Welcome to my web development portfolio website! My name is Yash and I am a web developer with experience in creating dynamic and responsive websites. On this site, you will find a selection of my previous work, including a variety of projects I have built using HTML, CSS, JavaScript, and various web development frameworks. I specialize in creating modern, user-friendly websites that are optimized for both desktop and mobile devices. If you have any questions or would like to discuss a potential project, please feel free to contact me. Thank you for visiting!
     </SectionText>
     <Button onClick={ () => window.location = 'https://google.com'}>Learn More</Button>
   </LeftSection>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,8 +2,8 @@ export const projects = [
   {
     title: 'Sonic Store',
     description: "A fully responsive ecommerce website with sanity and payments by stripe.",
-      image: '/image1/.png',
-      tags: ['Nextjs', 'Sanity', 'Stripe'],
+    image: 'https://user-images.githubusercontent.com/70088342/160780206-9cfe7c0a-3d8e-4a20-a055-b12efebe6c30.png',
+    tags: ['Nextjs', 'Sanity', 'Stripe'],
     source: 'https://github.com/YashVaghel/ecommerce-nextjs-sanity',
     visit: 'https://ecommerce-nextjs-sanity-omega.vercel.app/',
     id: 0,
@@ -11,30 +11,13 @@ export const projects = [
   {
     title: 'CryptoVerse',
     description:"All in one cryptocurrency website for tracking all cryptocurrencies in world",
-    image: '/images1/.png',
+    image: 'https://camo.githubusercontent.com/06005647905522c1c8a706b5b54b42199822f022a8857491f64bbd2c90eefde8/68747470733a2f2f692e6962622e636f2f386768354a63382f696d6167652e706e67',
     tags: ['React', 'JavaScript','RapidApi'],
     source: 'https://github.com/YashVaghel/Cryptoverse',
-    visit: 'https://google.com',
+    visit: 'https://cryptoverse-tau.vercel.app/',
     id: 1,
   },
-  {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
-  },
-  {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
-  },
+
 ];
 
 export const TimeLineData = [
