bf21642e0d41fd865187ba4145c5727ba36de89b
DHRUV PATEL <69008045+dhruvpatel-alt@users.noreply.github.com>
2023-03-17 20:42:14 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = (props) => (
       My Personal Portfolio
     </SectionTitle>
     <SectionText>
-    Welcome to my web development portfolio website! My name is Dhruv and I am a web developer with experience in creating dynamic and responsive websites. On this site, you will find a selection of my previous work, including a variety of projects I have built using HTML, CSS, JavaScript, and various web development frameworks. I specialize in creating modern, user-friendly websites that are optimized for both desktop and mobile devices. If you have any questions or would like to discuss a potential project, please feel free to contact me. Thank you for visiting!
+    Welcome to my web development portfolio website! My name is Dhruv, and I am an experienced web developer with a passion for creating dynamic and responsive websites. I specialize in building modern, user-friendly websites that are optimized for both desktop and mobile devices. Here, you can find a selection of my previous work, including a variety of projects that I have built using React JS, Node JS, and various web development frameworks. In addition, I have extensive experience in creating APIs using Python Flask, which can add more features and functionalities to website. If you have any questions , please do not hesitate to contact me. Thank you for taking the time to visit my website!
     </SectionText>
     <Button onClick={ () => window.location = 'https://google.com'}>Learn More</Button>
   </LeftSection>
