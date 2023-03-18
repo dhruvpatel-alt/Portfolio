0ba812861bc2994d3452ce61641ac5f859afc865
DHRUV PATEL <69008045+dhruvpatel-alt@users.noreply.github.com>
2023-03-19 01:21:42 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,12 +10,12 @@ export const projects = [
     id: 1,
   },
   {
-    title: 'iNotebook',
-    description:"INotebook is a cloud-based, fully responsive React application designed for personal note-taking, featuring integrated JWT-based authentication powered by Node.js.",
-    image: 'https://user-images.githubusercontent.com/69008045/225899925-13413a3c-a5a3-4493-a25f-fecd3a02cdbd.png',
-    tags: ['React', 'Bootstrap','NodeJs'],
-    source: 'https://github.com/dhruvpatel-alt/iNotebook_frontend',
-    visit: 'https://i-notebook-frontend-nine.vercel.app/',
+    title: 'iLocate',
+    description:"ILocate is a fully responsive React application that enables users to upload images of  a specific Place. It is designed to retrieve the location on a map based on the address.",
+    image: 'https://user-images.githubusercontent.com/69008045/226133730-0441f07e-0a53-4d8e-8d49-5a5e45dffebc.png',
+    tags: ['React','NodeJs','Mapbox'],
+    source: 'https://github.com/dhruvpatel-alt/iLocate',
+    visit: 'https://ilocate.netlify.app/',
     id: 1,
   },{
     title: 'SP NEWS App',
